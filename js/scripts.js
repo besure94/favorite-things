85e363b3bc3b8ee26855df98a506fbca0c3c8f90
Brian Scherner <brianscherner@gmail.com>
2023-05-16 13:15:38 -0700

    Add eventListener for submit function.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// window.addEventListener("load", function() {
+// 	let form = document.querySelector("form");
+// });
+
+form.addEventListener("submit", function(event) {
+	let boxOne = document.getElementById("boxOne").value.toLowerCase();
+	let boxTwo = document.getElementById("boxTwo").value.toLowerCase();
+	let boxThree = document.getElementById("boxThree").value.toLowerCase();
+});
