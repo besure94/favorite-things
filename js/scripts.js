07a3a1a17f6b6f72234f043bf695241c70f52540
Brian Scherner <brianscherner@gmail.com>
2023-05-17 10:39:57 -0700

    Define new array and push each array element from old array to new array. Create five list elements and append each one to new array. Select ul element and append each array element to list.

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,35 @@ window.addEventListener("load", function() {
 		let boxFour = document.getElementById("boxFour").value;
 		let boxFive = document.getElementById("boxFive").value;
 		let answers = [boxOne, boxTwo, boxThree, boxFour, boxFive];
-		console.log(answers);
+		let newAnswersArray = [];
+		newAnswersArray.push(answers[1]);
+		newAnswersArray.push(answers[0]);
+		newAnswersArray.push(answers[2]);
+		newAnswersArray.push(answers[3]);
+		newAnswersArray.push(answers[4]);
 
+		//come back and refactor later//
+		const li1 = document.createElement("li");
+		const li2 = document.createElement("li");
+		const li3 = document.createElement("li");
+		const li4 = document.createElement("li");
+		const li5 = document.createElement("li");
+		li1.append(newAnswersArray[0]);
+		li2.append(newAnswersArray[1]);
+		li3.append(newAnswersArray[2]);
+		li4.append(newAnswersArray[3]);
+		li5.append(newAnswersArray[4]);
+		const firstUl = document.querySelector("ul");
+		firstUl.append(li1);
+		firstUl.append(li2);
+		firstUl.append(li3);
+		firstUl.append(li4);
+		firstUl.append(li5);
+
+
+		console.log("answers: ", answers);
+		console.log("newAnswersArray: ", newAnswersArray);
+		console.table(newAnswersArray);
 
 
 		event.preventDefault();
